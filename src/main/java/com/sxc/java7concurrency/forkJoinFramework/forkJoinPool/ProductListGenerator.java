ce641691ab179beed039a7ff59683790a90215ed
Xuecheng Shan <shanxuecheng@gmail.com>
2015-11-25 17:55:56 +0800

    test fork/join framework
    test fork/join framework

---
+++ src/main/java/com/sxc/java7concurrency/forkJoinFramework/forkJoinPool/ProductListGenerator.java
@@ -0,0 +1,18 @@
+package com.sxc.java7concurrency.forkJoinFramework.forkJoinPool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductListGenerator {
+
+    public List<Product> generate(int size) {
+        List<Product> ret = new ArrayList<Product>();
+        for (int i = 0; i < size; i++) {
+            Product product = new Product();
+            product.setName("Product " + i);
+            product.setPrice(10);
+            ret.add(product);
+        }
+        return ret;
+    }
+}
